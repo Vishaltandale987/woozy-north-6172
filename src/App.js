edf19cf04fc60c732bfc8326aa2b945677124f3c
yash <yash10072000@gmail.com>
2022-12-14 22:39:28 +0530

    added file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Plan from './pages/plan/Plan';
 
 function App() {
   return (
     <div className="App">
-      <h1>-woozy-north-6172</h1>
+      <Plan />
     </div>
   );
 }
