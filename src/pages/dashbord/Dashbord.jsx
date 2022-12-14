79d49a282f14fa34db345e3baf70da913bafad6f
Vishaltandale987 <tandalevishal987@gmail.com>
2022-12-14 22:55:50 +0530

    dashbord

---
+++ src/pages/dashbord/Dashbord.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Dashbord() {
+  return (
+    <div>
+        <img src="" alt="" />
+        <img src="" alt="" />
+    </div>
+  )
+}
+
+export default Dashbord
