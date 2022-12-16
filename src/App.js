0c1a17a331151e943c940f7190612bfb12d4de0c
yash <yash10072000@gmail.com>
2022-12-16 22:09:52 +0530

    added file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import NewSurvey from './pages/CreateSurvey/NewSurvey';
 
 function App() {
   return (
     <div className="App">
-      <h1>-woozy-north-6172</h1>
+      <NewSurvey/>
     </div>
   );
 }
